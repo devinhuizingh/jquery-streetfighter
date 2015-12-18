c85d32331a2ad54fdd80f604330c77bb31b9aace
devinhuizingh <devinhuizingh@gmail.com>
2015-12-18 15:34:33 -0700

    done with glitch

--- js/app.js
+++ js/app.js
@@ -1,61 +1,58 @@
 $(document).ready(function () {
 
-$
-('.ryu').mouseenter(function() {
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
-})
-.mouseleave(function() {
-	$('.ryu-ready').hide();
-	$('.ryu-still').show();
-})
+	$
+	('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
 
-.mousedown(function() {
-	console.log('in mousedown');
-	playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show()
-    $('.hadouken').finish().show().animate(
-  {'left': '1060px'},
-  500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '560px');
-  }
-);
+	.mousedown(function() {
+		console.log('in mousedown');
+		playHadouken();
+	    $('.ryu-ready').hide();
+	    $('.ryu-throwing').show();
+	    $('.hadouken').show()
+	    $('.hadouken').finish().show().animate(
+	  {'left': '1060px'},
+	  500,
+	  function() {
+	    $(this).hide();
+	    $(this).css('left', '560px');
+	  }
+	);
 
-})  	
-  
-.mouseup(function() {
-	console.log('in mouseup');
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-    
-      });
+	})  	
+	  
+	.mouseup(function() {
+		console.log('in mouseup');
+	    $('.ryu-throwing').hide();
+	    $('.ryu-ready').show();
+	    
+	      });
 
 
 
-});
 
-$(document).keydown(function(event) {
-	if (event.which === 88) {
-	$('.ryu-still').hide()
-	$('.ryu-cool').show();
-	;
-};
+	$(document).keydown(function(event) {
+		if (event.which === 88) {
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		
+		}
 
-$(document).keydown(function(event) {
-	if (event.which === 88) {
-	$('.ryu-still, .ryu-ready').hide();
-	$('.ryu-cool').show();
-	
-}
-}).keyup(function(event) {
-	if (event.which === 88) {
-	$('.ryu-cool, .ryu-ready').hide();
-	$('.ryu-still').show();
-}});
+
+	}).keyup(function(event) {
+		
+		if (event.which === 88) {
+			console.log('keyup active');
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
 
 
 });
